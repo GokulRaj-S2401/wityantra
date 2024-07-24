cbca64f4ee21bbbc5a96d018810faab8755a3bd3
gokulraj.sivaraj <gokulraj.sivaraj@zoifintech.com>
2024-07-24 19:58:57 +0530

    'adf'

---
+++ baseUrl.js
@@ -0,0 +1,12 @@
+window.globalConfig = {
+  // baseUrl: "https://zapp-dev.zoiftspl.com/CyberbeatAPI",
+  // baseUrl: 'https://uatapi.autharc.io',
+  baseUrl: 'https://demomiriapi.zoiftspl.com',
+  Android: "https://www.cyberbeat.com.sg/",
+  IOS: "https://www.cyberbeat.com.sg/",
+  UserImage: "https://demomiriapi.zoiftspl.com/Images/UserImage/",
+  LoginImage: "https://demomiriapi.zoiftspl.com/Images/LoginUserImage/",
+  Image: "https://demomiriapi.zoiftspl.com/",
+  Ecommerce: "https://ecommerse.zoiftspl.com/cart",
+  FundTransfer: "https://cyberbeatbanking.zoiftspl.com/fundtransfer"
+};
